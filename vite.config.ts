af888d6608b1f2f7f683c303d52b1caab015313e
Elliot Sayes <elliot@sayes.cloud>
2024-08-28 15:17:58 +0800

    Configure vite comlink plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { comlink } from 'vite-plugin-comlink'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [comlink(), react()],
+  worker: {
+    plugins: () => [comlink()]
+  }
 })
